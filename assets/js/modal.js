bed592539386f33b975506ad297f260153897325
matamata-dayo <k.matamata.macbook@icloud.com>
2022-11-01 23:47:43 +0900

    8day

---
+++ assets/js/modal.js
@@ -0,0 +1,23 @@
+const buttonOpen = document.getElementById('modalOpen');
+const modal = document.getElementsByClassName('signUpModal');
+const buttonClose = document.getElementsByClassName('modalCloseButton')[0];
+
+// ボタンがクリックされた時
+buttonOpen.addEventListener('click', modalOpen);
+function modalOpen() {
+  modal.style.display = 'block';
+}
+
+// バツ印がクリックされた時
+buttonClose.addEventListener('click', modalClose);
+function modalClose() {
+  modal.style.display = 'none';
+}
+
+// モーダルコンテンツ以外がクリックされた時
+addEventListener('click', outsideClose);
+function outsideClose(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
--- assets/js/selsectbox.js
+++ assets/js/selsectbox.js
@@ -3,26 +3,26 @@ let selectTeam = document.getElementById('select-box-team');
 selectTeam.onchange = function() {
 
   const img = [
-    "../../assets/img/teams/Arsenal.jpeg", 
-    "../../assets/img/teams/Chelsea.jpeg",
-    "../../assets/img/teams/Liverpool.jpeg",
-    "../../assets/img/teams/Manchester-city.jpeg",
-    "../../assets/img/teams/Manchester-united.jpg",
-    "../../assets/img/teams/Tottenham.jpeg",
-    "../../assets/img/teams/FW_img.png"
+    '../../assets/img/teams/Arsenal.jpeg', 
+    '../../assets/img/teams/Chelsea.jpeg',
+    '../../assets/img/teams/Liverpool.jpeg',
+    '../../assets/img/teams/Manchester-city.jpeg',
+    '../../assets/img/teams/Manchester-united.jpg',
+    '../../assets/img/teams/Tottenham.jpeg',
+    '../../assets/img/teams/FW_img.png'
   ];
 
-  if (selectTeam.value === "Arsenal") {
+  if (selectTeam.value === 'Arsenal') {
     document.getElementById('team-img').src = img[0];
-  } else if (selectTeam.value === "Chelsea") {
+  } else if (selectTeam.value === 'Chelsea') {
     document.getElementById('team-img').src = img[1];
-  } else if (selectTeam.value === "Liverpool") {
+  } else if (selectTeam.value === 'Liverpool') {
     document.getElementById('team-img').src = img[2];
-  } else if (selectTeam.value === "Manchester-City") {
+  } else if (selectTeam.value === 'Manchester-City') {
     document.getElementById('team-img').src = img[3];
-  } else if (selectTeam.value === "Manchester-United") {
+  } else if (selectTeam.value === 'Manchester-United') {
     document.getElementById('team-img').src = img[4];
-  } else if (selectTeam.value === "Tottenham-Hotspur") {
+  } else if (selectTeam.value === 'Tottenham-Hotspur') {
     document.getElementById('team-img').src = img[5];
   }
 }
@@ -32,19 +32,19 @@ let selectPosition = document.getElementById('select-box-position');
 selectPosition.onchange = function() {
 
   const img = [
-    "../../assets/img/positions/FW_img.png", 
-    "../../assets/img/positions/MF_img.png",
-    "../../assets/img/positions/DF_img.png",
-    "../../assets/img/positions/GK_img.png"
+    '../../assets/img/positions/FW_img.png', 
+    '../../assets/img/positions/MF_img.png',
+    '../../assets/img/positions/DF_img.png',
+    '../../assets/img/positions/GK_img.png'
   ];
 
-  if (selectPosition.value === "fw") {
+  if (selectPosition.value === 'fw') {
     document.getElementById('position-img').src = img[0];
-  } else if (selectPosition.value === "mf") {
+  } else if (selectPosition.value === 'mf') {
     document.getElementById('position-img').src = img[1];
-  } else if (selectPosition.value === "df") {
+  } else if (selectPosition.value === 'df') {
     document.getElementById('position-img').src = img[2];
-  } else if (selectPosition.value === "gk") {
+  } else if (selectPosition.value === 'gk') {
     document.getElementById('position-img').src = img[3];
   }
 }
