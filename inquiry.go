7b2d4b3c1670679a9c718f2c16dc62b082c58213
matamata-dayo <k.matamata.macbook@icloud.com>
2022-11-11 18:40:51 +0900

    15day++

--- inquiry.go
+++ inquiry.go
@@ -32,17 +32,20 @@ func handleInquiry(w http.ResponseWriter, r *http.Request) {
 		mailCheckLen := utf8.RuneCountInString(r.Form.Get("mailCheck"))
 		questionLen := utf8.RuneCountInString(r.Form.Get("question"))
 		if nameLen == 0 {
-			inruiryFormMessage.NameError = inruiryFormMessage.NameError + "「お名前」が入力されていません。"
-		} else if mailLen == 0 {
-			inruiryFormMessage.MailError = inruiryFormMessage.MailError + "「メールアドレス」が入力されていません。"
-		} else if mailCheckLen == 0 {
-			inruiryFormMessage.MailCheckError = inruiryFormMessage.MailCheckError + "「メールアドレス確認」が入力されていません。"
-		} else if questionLen == 0 {
-			inruiryFormMessage.QuestionError = inruiryFormMessage.QuestionError + "「お問い合わせ内容」が入力されていません。"
+			inruiryFormMessage.NameError = inruiryFormMessage.NameError + "お名前が入力されていません。"
+		}
+		if mailLen == 0 {
+			inruiryFormMessage.MailError = inruiryFormMessage.MailError + "メールアドレスが入力されていません。"
+		}
+		if mailCheckLen == 0 {
+			inruiryFormMessage.MailCheckError = inruiryFormMessage.MailCheckError + "メールアドレス確認が入力されていません。"
+		}
+		if questionLen == 0 {
+			inruiryFormMessage.QuestionError = inruiryFormMessage.QuestionError + "お問い合わせ内容が入力されていません。"
 		}
 
 		// メールアドレス一致チェック
-		if r.Form.Get("mail") != r.Form.Get("mailCheck") {
+		if inruiryFormMessage.MailError == "" && r.Form.Get("mail") != r.Form.Get("mailCheck") {
 			inruiryFormMessage.MailError = inruiryFormMessage.MailError + "メールアドレスが一致していません。"
 		}
 
